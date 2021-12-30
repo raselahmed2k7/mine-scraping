4c7260106dfa6f2a55b47931a4bf320b9198999d
MD Tawab Alam Khan <cs.antorkhan@gmail.com>
2021-12-30 11:51:26 +0600

    Updated Staging Server IP

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -4,7 +4,7 @@ set :stage, :development
 # Defines a single server with a list of roles and multiple properties.
 # You can define all roles on a single server, or split them:
 set :bundle_without, %w{test}.join(':')
-server "52.197.33.79", user: "ubuntu", roles: %w{app db web}
+server "52.194.207.93", user: "ubuntu", roles: %w{app db web}
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
 
@@ -18,10 +18,10 @@ server "52.197.33.79", user: "ubuntu", roles: %w{app db web}
 # property set. Specify the username and a domain or IP for the server.
 # Don't use `:all`, it's a meta role.
 
-role :app, %w{ubuntu@52.197.33.79}
+role :app, %w{ubuntu@52.194.207.93}
 # role :web, %w{user1@primary.com user2@additional.com}, other_property: :other_value
 # role :db,  %w{deploy@example.com}
-set :branch, ENV['BRANCH'] || 'main'
+set :branch, ENV['BRANCH'] || 'develop'
 
 
 # Configuration
